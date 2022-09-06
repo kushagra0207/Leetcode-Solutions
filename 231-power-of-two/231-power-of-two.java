0916dafda74e6c1caf813eeb7d5593840b4828d2
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2022-09-06 09:39:49 +0530

    Time: 2 ms (71.20%), Space: 41.1 MB (66.17%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
       if(n==0) return false;
-      while(n%2==0) n/=2;
-      if(n==1) return true;
-      return false;
-        
+      long x = (long) n;
+      return (x&(x-1))==0;
     }
 }
