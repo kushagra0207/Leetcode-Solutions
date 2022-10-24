b2508a7512692afc0a6d8431a0ec2bbfc8d848b3
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2022-10-24 22:16:28 +0530

    Added solution - LeetHub

---
+++ Earthquake and the Paint Shop - GFG/earthquake-and-the-paint-shop.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            String s[] = new String[N];
+            for (int i = 0; i < N; i++) s[i] = read.readLine();
+            Solution ob = new Solution();
+            alphanumeric ans[] = ob.sortedStrings(N, s);
+            for (int i = 0; i < ans.length; i++)
+                System.out.println(ans[i].name + " " + ans[i].count);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class alphanumeric {
+    public String name;
+    public int count;
+    alphanumeric(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+};
+class Solution {
+    alphanumeric[] sortedStrings(int N, String A[]) {
+        
+        Map<String, Integer>  map =new TreeMap<>(); //O(n)
+
+        for(int  i=0; i<N; i++) //O(n)
+        {
+            map.put(A[i], map.getOrDefault(A[i], 0)+1); //O(logn)
+        }
+        int n = map.size();
+
+        alphanumeric  ans[] = new alphanumeric[n] ; // int arr[] =new int[n];
+        int ind=0;
+
+
+        for(Map.Entry<String, Integer> ent : map.entrySet())
+        {
+            ans[ind++] =  new alphanumeric(ent.getKey(), ent.getValue());
+        }
+
+        return ans;
+    }
+};
