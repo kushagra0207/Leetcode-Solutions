567c5088a599ffc81ffeacfeb5003362dd7583a1
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2022-11-01 22:19:02 +0530

    Added solution - LeetHub

---
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S[] = read.readLine().split(" ");
+            int R = Integer.parseInt(S[0]);
+            int C = Integer.parseInt(S[1]);
+            int matrix[][] = new int[R][C];
+            int c = 0;
+            for(int i = 0; i < R; i++){
+                String line[]=read.readLine().trim().split(" ");
+                for(int j = 0; j < C; j++){
+                    matrix[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.median(matrix, R, C);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int median(int matrix[][], int R, int C) {
+        // code here   
+          int min = Integer.MAX_VALUE;
+        int max=  Integer.MIN_VALUE;
+
+        for(int i=0 ;i<R; i++){ 
+            if(matrix[i][0] < min){
+            min = matrix[i][0];}
+
+            if(matrix[i][C-1] > max){
+            max  = matrix[i][C-1];}
+        }
+         int count = (R*C+1)/2;
+
+        while(min<max)
+        {
+            int mid = (min+max)/2;
+            int curr_count = 0;
+            for(int i =0 ; i<R; i++)
+            {
+                int l=0; int r =C-1;
+                while(l<r)
+                {
+                    int m = (l+r)/2;
+                    if(matrix[i][m] <= mid)
+                    l=m+1;
+
+                    else
+                    r=m;
+                }
+                if(matrix[i][l] <=mid) // searching 5 in row (1,3,y) : y<=mid
+                {
+                    curr_count++;
+                }
+                curr_count+=l ;
+            }
+            if(curr_count < count)
+            {
+                min =  mid+1;
+            }
+
+            else
+            max=mid;
+
+
+        }
+        return min;
+    }
+}
