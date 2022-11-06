64f56d404be43a6be87137f8179d80d4f6bffb53
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2022-11-07 00:16:08 +0530

    Time: 1 ms (58.93%), Space: 42.6 MB (44.91%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortColors(int[] nums) {
+            
+            // Kushagra Gupta - Programmer Specialist
+        // color = red || white || blue;
+            
+            // color red =0;
+            // color white = 1;
+            // color blue= 2;
+            
+            
+//             Method - 1 By using Arrays.sort method but will not do as per the question.
+            Arrays.sort(nums);
+            
+                    
+    }
+        
+}
