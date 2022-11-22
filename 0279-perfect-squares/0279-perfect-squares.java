f8e05d221ab685dfd1d9943b86f6830c29984d88
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2022-11-22 21:01:57 +0530

    Time: 1 ms (100.00%), Space: 39.2 MB (99.59%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numSquares(int n) {
+        while(n%4 == 0)  n /= 4;
+        if(n%8 == 7) return 4;
+        for(int x=0; x*x <=n; x++){
+            int y = (int)Math.sqrt(n - x*x);
+            if(x*x + y*y == n){
+                if(x == 0 || y == 0) return 1;
+                else return 2;
+            }
+        }
+        return 3;
+    }
+}
