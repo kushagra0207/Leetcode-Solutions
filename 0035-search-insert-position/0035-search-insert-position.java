e8b89ab3f79654aeae374a19951d16206e086d7c
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2022-12-04 12:23:51 +0530

    Time: 0 ms (100.00%), Space: 43 MB (75.54%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,27 +1,15 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int start=0;// Kushagra Gupta Java Developer
-	   int end=nums.length-1;
-	 
-	    int res=0;
-        //If target less than our first element,new element will be placed at 0th index
-        if(target<nums[0]){
-            return 0;
-        }
-         while(start<=end) {
-              int mid=start+(end-start)/2;
-
-              if(nums[mid]==target) {
-                  return mid;
-              }else if(nums[mid]<target){
-                  start=mid+1;
-                
-              }else if(nums[mid]>target) {
-                  end=mid-1;
-
-              }
-          }
-        return end+1;
-        
+        int q = 0, w = nums.length-1,r=-1;
+while (q <= w) {
+r = (w + q) >> 1;
+if (nums[r] > target)
+w = r - 1;
+else if (target > nums[r])
+q = r + 1;
+else
+return r;
+}
+return nums[r]>target?r:r+1;
     }
 }
