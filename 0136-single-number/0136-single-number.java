cd07faec2b2ce749b77aae81bb4852ea8a723821
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2022-12-17 07:50:35 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (99.72%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int Kushagra=0;
-    for(int i=0;i<nums.length;i++){
-        Kushagra=Kushagra^nums[i];  // LC Kushagra 1702
-    }
-    return Kushagra;
-        
-    }
+  public int singleNumber(int[] nums) {
+    int ans = 0;
+
+    for (final int num : nums)
+      ans ^= num;
+
+    return ans;
+  }
 }
