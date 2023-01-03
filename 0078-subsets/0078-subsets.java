b123654111231e7a97df94c2534deb53be7fce34
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2023-01-03 09:59:13 +0530

    Time: 2 ms (35.86%), Space: 43.1 MB (52.29%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-         List<List<Integer>> ans = new ArrayList<>();
-    dfs(nums, 0, new ArrayList<>(), ans);
-    return ans;
-  }
-
-  private void dfs(int[] nums, int s, List<Integer> path, List<List<Integer>> ans) {
-    ans.add(new ArrayList<>(path));
-
-    for (int i = s; i < nums.length; ++i) {
-      path.add(nums[i]);
-      dfs(nums, i + 1, path, ans);
-      path.remove(path.size() - 1);
+         List<List<Integer>> answer = new ArrayList<>();
+        backtrace(nums,0,new ArrayList<Integer>(),answer);
+        return answer;
+        
     }
+    public void backtrace(int[] nums,int start, List<Integer> track,List<List<Integer>> answer){
+        answer.add(new ArrayList<>(track));
+        for(int i=start;i<nums.length;i++){
+            
+            track.add(nums[i]);
+            backtrace(nums,i+1,track,answer);
+            track.remove(track.size()-1);
+        
+        }
     }
+   
 }
