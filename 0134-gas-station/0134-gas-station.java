a1c146b5ff69d896a72037678f1dbde4243280a4
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2023-01-08 00:29:56 +0530

    Time: 1 ms (100.00%), Space: 62.3 MB (88.08%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,29 +1,26 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-         // Initialize totalGas and totalCost to 0
-        int totalGas = 0, totalCost = 0;
-        // Iterate through all the gas stations
-        for(int i = 0 ; i<gas.length; i++){
-            // Add the gas and cost at each station to the total
-            totalGas += gas[i];
-            totalCost += cost[i];
-        }
-        // If totalCost is greater than totalGas, it is not possible to complete the circuit
-        if(totalGas < totalCost) return -1;
         
-        // Initialize remainsGas and start to 0
-        int remainsGas = 0, start = 0;
-        // Iterate through all the gas stations
-        for(int i = 0 ; i < gas.length; i++){
-            // Add the difference between gas and cost at each station to remainsGas
-            remainsGas = remainsGas +(gas[i] - cost[i]);
-            // If remainsGas becomes negative, set start to the next station and reset remainsGas to 0
-            if(remainsGas < 0 ){
-                start = i+1;
-                remainsGas = 0;
+        int gas_sum=0;
+        int cost_sum=0;
+        int count=0;
+        int start =0;
+         for(int i=0;i<gas.length;i++){
+             gas_sum=gas_sum+gas[i];
+             cost_sum=cost_sum+cost[i];
+         }
+        if(gas_sum<cost_sum) return -1;
+        
+        for(int i=0;i<gas.length;i++){
+            count=count+(gas[i]-cost[i]);
+            if(count<0){
+                count=0;
+                start=i+1;
             }
-        }
-        // Return the starting station
+            
+            
+}
+        
         return start;
     }
 }
