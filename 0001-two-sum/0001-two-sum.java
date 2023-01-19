8a3feb4f0b08c06e3bc665a31ab2ddb3c7b59c14
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2023-01-19 22:07:26 +0530

    Time: 4 ms (76.12%), Space: 42.8 MB (49.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,51 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int a =0;
-       int b=0;
         
+        /* Writer Kushagra Gupta done - 19 January 2023 
+        
+        Method 1 */
+        
+//         int a =0;
+//        int b=0;
+        
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]+nums[j]==target){
+//                     a=i;
+//                      b =j;
+//                 }
+//             }
+//         }
+//         int[] arr = new int[2];
+//         arr[0]=a;
+//         arr[1]=b;
+//         return arr;
+        
+        
+        // Mathod 2 using HashMap
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    a=i;
-                     b =j;
-                }
-            }
+            map.put(nums[i],i);
         }
-        int[] arr = new int[2];
-        arr[0]=a;
-        arr[1]=b;
-        return arr;
+        for(int i=0;i<nums.length;i++){
+            int rem = target-nums[i];
+            if(map.containsKey(rem)){
+                
+                int aer = map.get(rem);
+                if(aer==i){continue;}
+
+                return new int[] {map.get(rem),i};
+
+}
+            
+        }
+        
+        
+        
+        
+        
+        return new int[]{};
       
         
     }
