2216643a23abd9eeb6d7a0c603e85c18a12f226b
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2023-01-19 21:15:10 +0530

    Time: 38 ms (51.87%), Space: 42.3 MB (80.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
         int a =0;
-        int b =0;
+       int b=0;
         
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
                     a=i;
-                    b =j;
+                     b =j;
                 }
             }
         }
@@ -16,6 +15,7 @@ class Solution {
         arr[0]=a;
         arr[1]=b;
         return arr;
+      
         
     }
 }
