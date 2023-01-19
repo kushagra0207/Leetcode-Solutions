7f3fb58a47037327622d181e099be06cca094d61
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2023-01-19 22:47:22 +0530

    Time: 2 ms (96.20%), Space: 42.3 MB (76.10%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -12,39 +12,60 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
         
-        // writer kushagra gupta
+        // writer kushagra gupta - 19 jan 2023
         
-        // Simple linked lIst question to involve
+        
+        // Simple linked list question to involve
         
         // using map and hashmap also we can do.
         
         // No need to worry and check.
+//         ListNode temp = new ListNode();
+//         ListNode curr = temp;
+        
+        
+//         int carry=0;
+        
+//         while(l1!=null||l2!=null){
+//             int x = (l1!=null)?l1.val:0;
+//             int y = (l2!=null)?l2.val:0;
+//             int sum=carry+x+y;
+//             carry = sum/10;
+//            curr.next =new ListNode(sum%10);
+//             curr = curr.next;
+//             if(l1!=null) l1 = l1.next;
+//             if(l2 !=null)  l2 = l2.next;
+            
+            
+            
+            
+            
+//         }
+//         if(carry>0){
+//             curr.next=new ListNode(carry);
+//         }
+         
+//         return temp.next;
+        
+        int carry =0;
+        
         ListNode temp = new ListNode();
-        ListNode curr = temp;
-        
-        
-        int carry=0;
-        
-        while(l1!=null||l2!=null){
-            int x = (l1!=null)?l1.val:0;
-            int y = (l2!=null)?l2.val:0;
-            int sum=carry+x+y;
+        ListNode curr=temp;
+        while(l1!=null || l2!=null){
+            int a = (l1!=null)?l1.val:0;
+            int b = (l2!=null)?l2.val:0;
+            int sum = a+b+carry;
             carry = sum/10;
-           curr.next =new ListNode(sum%10);
+             curr.next  =new ListNode(sum%10);
             curr = curr.next;
             if(l1!=null) l1 = l1.next;
-            if(l2 !=null)  l2 = l2.next;
-            
-            
-            
-            
-            
+            if(l2!=null)l2 = l2.next;
+        
         }
         if(carry>0){
-            curr.next=new ListNode(carry);
+            curr.next = new ListNode(carry);
+            curr = curr.next;
+            
         }
-         
         return temp.next;
-        
-    }
-}
+    }}
