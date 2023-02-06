fe6f6aad7e93b6db9c90561ce3d1edff9ca8bafb
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2023-02-06 14:09:17 +0530

    Time: 0 ms (100.00%), Space: 42 MB (98.38%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] result = new int[2*n];
-        for(int i=0;i<2*n;i++){
+        for (int i=0;i<2*n;i++){
             if(i%2==0){
-                result[i]=nums[i/2];
+                result[i]= nums[i/2];
             }
             else{
                 result[i]=nums[n+i/2];
             }
-
-        
         }
         return result;
+        
     }
 }
-// this is the question which you have solved today 
