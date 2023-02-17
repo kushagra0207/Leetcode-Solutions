96c0e96c831c83be9d595177bb4c0dd06f2b9ae4
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2023-02-17 23:51:28 +0530

    Added solution - LeetHub

---
+++ Is it Fibonacci - GFG/is-it-fibonacci.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long solve(int N, int K, vector<long long> GeekNum) {
+        // code here
+          long long sum=0;
+        sum=accumulate(GeekNum.begin(),GeekNum.end(),sum);
+        int j=0;
+        for(int i=K;i<N;i++){
+            GeekNum.push_back(sum);
+            sum+=sum;
+            sum-=GeekNum[j];
+            j++;
+        }
+        return GeekNum[N-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, K;
+        cin >> N >> K;
+
+        vector<long long> GeekNum(K);
+
+        for (int i = 0; i < K; i++) cin >> GeekNum[i];
+
+        Solution ob;
+        cout << ob.solve(N, K, GeekNum) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
